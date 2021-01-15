bb4f83ee868a1e31fb60cb659fee8d003d435e51
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2021-01-15 17:43:21 -0500

    offset changes and probe margin increased

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -994,7 +994,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 10000
@@ -1131,7 +1131,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 15
+#define Y_MIN_POS 10
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS (Y_BED_SIZE + Y_MIN_POS)
@@ -1377,7 +1377,7 @@
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
 // #define MANUAL_X_HOME_POS 0
-#define MANUAL_Y_HOME_POS -20
+#define MANUAL_Y_HOME_POS -10
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
