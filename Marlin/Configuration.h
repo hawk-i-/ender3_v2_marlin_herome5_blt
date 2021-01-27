ee200a5f8204fb49b26148bccde7d48607ce390e
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2021-01-27 18:09:02 -0500

    fixes with shifted 4 motion on printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -989,7 +989,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET {  -45, 8, 0 }
+#define NOZZLE_TO_PROBE_OFFSET {  -45, 5, 0 }
 //#define NOZZLE_TO_PROBE_OFFSET {  38.5, 0, 0 } //Bondtech BMG Bltouch
 
 // Most probes should stay away from the edges of the bed, but
@@ -1126,15 +1126,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 20
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS (Y_BED_SIZE + Y_MIN_POS)
+#define Y_MAX_POS Y_BED_SIZE
 #define Z_MAX_POS 240
 
 /**
@@ -1377,7 +1377,7 @@
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
 // #define MANUAL_X_HOME_POS 0
-#define MANUAL_Y_HOME_POS -10
+#define MANUAL_Y_HOME_POS 10
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
